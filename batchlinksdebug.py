d252176f56cfac2292674b4fa5de325a558cfb96
wfq1971 <90599179+wfq1971@users.noreply.github.com>
2023-09-27 13:56:07 +0800

    Update batchlinksdebug.py

--- batchlinksdebug.py
+++ batchlinksdebug.py
@@ -1,6 +1,6 @@
 import os, subprocess, sys, shlex, pickle
 
-curdir = '/content'
+curdir = '/common'
 branch = '-b uiupdate+aria ' #'-b ariacivitnew ' <-space in the end
 linetoexecute = [
     'git clone -b v2.1 https://github.com/camenduru/stable-diffusion-webui /common/cswpf', 
