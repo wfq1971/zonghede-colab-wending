c1daa3a5aef5056bf9dff3295a5b4e56fe46d6f1
wfq1971 <90599179+wfq1971@users.noreply.github.com>
2023-09-27 13:26:29 +0800

    Update setuptimer.py

--- setuptimer.py
+++ setuptimer.py
@@ -1,7 +1,7 @@
 import os, shutil
 curdir = os.path.dirname(os.path.realpath(__file__))
-os.makedirs('/content/volatile-concentration-localux/static', exist_ok=True)
-timerscriptdir = '/content/volatile-concentration-localux/extensions-builtin/nocrypt-colab-timer/javascript'
+os.makedirs('/common/cswpf/static', exist_ok=True)
+timerscriptdir = '/common/cswpf/extensions-builtin/nocrypt-colab-timer/javascript'
 os.makedirs(timerscriptdir, exist_ok=True)
 timerscriptpath = os.path.join(timerscriptdir, 'colab-timer.js')
 if not os.path.exists(timerscriptpath):
